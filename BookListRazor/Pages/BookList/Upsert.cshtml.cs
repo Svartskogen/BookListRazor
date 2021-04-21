48b1bcd73b863b65cb259f287d2f389801e98902
Svartskogen <amichelidebard@gmail.com>
2021-04-20 22:17:22 -0300

    Agregar archivos de proyecto.

---
+++ BookListRazor/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class UpsertModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if(id == null)
+            {
+                //create
+                return Page();
+            }
+
+            //update
+            Book = await _db.Book.FindAsync(id);
+            if(Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if(Book.Id == 0)
+                {
+                    _db.Book.Add(Book);
+                }
+                else
+                {
+                    _db.Book.Update(Book);
+                }
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
